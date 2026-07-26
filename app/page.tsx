f2a4e44141849b36759054cfb87d70ff42c0a7e8
Culinary Cookout <culinarycookout@gmail.com>
2026-07-26 16:07:39 -0700

    fix menu import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
 import Image from 'next/image';
-import Menu from './components/Menu';
+import Menu from '../components/Menu';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white p-4">
-      <div className="max-w-6xl mx-auto">
-        {/* Logo */}
-        <div className="flex justify-center mb-6">
-          <Image
-            src="/Culinary Cookout Logo.PNG"
-            alt="Culinary Cookout Logo"
-            width={200}
-            height={80}
-            className="h-24 md:h-32 w-auto object-contain"
-            priority
-          />
-        </div>
-        <Menu />
-      </div>
-    </div>
+    <main className="min-h-screen bg-black text-white p-4 md:p-8">
+      <Menu />
+    </main>
   );
 }
