0ec660d5be10763155b0afdeb095d7402a381f7e
Culinary Cookout <culinarycookout@gmail.com>
2026-07-27 13:25:04 -0700

    logoooo

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen bg-black text-white p-4">
       <div className="container max-w-2xl mx-auto">
+        {/* ✅ Logo – 200% BIGGER */}
         <div className="flex flex-col items-center justify-center w-full mb-6 pt-2">
           <img
             src="https://iili.io/CeCmPWJ.png"
             alt="Culinary Cookout"
-            className="h-20 md:h-24 w-auto object-contain"
+            className="h-48 sm:h-56 md:h-72 w-auto object-contain"
           />
         </div>
 
